8aad790d547ad6ee28996c09ad786f011bd1af4c
siwetzel <wetzel@math.uni-frankfurt.de>
2023-11-09 10:30:34 +0100

    Transfomration working without checking if the transformation is correct

--- transformation_0_1/transform_codes_to_0_1.R
+++ transformation_0_1/transform_codes_to_0_1.R
@@ -1,3 +1,32 @@
+library(stringr)
+library(readr)
+
+check_value = function(value, value_set, check_for_zero) {
+  # check if a coded value belongs to a certain value set
+  # if check_for_zero is true, it is enough that one value is in the set to return true
+  
+  # value could contain a concatenated string of more than one code
+  # first, any existing spaces are removed
+  # next, a list with all codes is stored in values (could also be a list with just one element)
+  values = unlist(strsplit(str_replace_all(value, " ", ""), ","))
+  
+  in_set = TRUE
+  
+  for (code in values) {
+    if (check_for_zero && code %in% value_set) {
+      return(TRUE)
+    }
+    if (!(code %in% value_set)) {
+      in_set = FALSE
+    }
+  }
+  
+  return(in_set)
+  
+}
+
+
+
 transform_codes <- function(){
   # TODO: add data as input later to function again
   # TODO: remove later, just for debugging purposes
@@ -19,6 +48,7 @@ transform_codes <- function(){
     "a1_4_1",
     "a1_4_2",
     "a1_4_3",
+  
     "a1_4_4",
     "a1_5",
     "a1_6",
@@ -62,49 +92,50 @@ transform_codes <- function(){
   # right column: values that are transformed to 0
   # tasks in A1
   rules_a1 = matrix(list(
-    (c(2)), c(1,0),
-    c(2), c(1,0),
-    c(1), c(2,0),
+    c("2"), c("1","0"),
+    c("2"), c("1","0"),
+    c("1"), c("2","0"),
     c("k"), c("sp","i","g","nl","str","s","ab"),
-    c("k","1","2","sp"), c("2","4","kl","g","x","y","s","div","ab"),
+    c("k","1","2","sp"), c("3","4","5","kl","g","x","y","s","div","ab"),
     c("k","fk","ff","ub","d","n"), c("f"),
-    c(1), c(2,0),
-    c(2), c(1,0),
-    c(1), c(2,0),
-    c(2), c(1,0),
+    c("1"), c("2","0"),
+    c("2"), c("1","0"),
+    c("1"), c("2","0"),
+    c("2"), c("1","0"),
     c("k"), c("d","f","u"),
     c(""), c("1,2,3,4,5"), # todo: wie geh ich hier mit allen Fällen um? Muss ich das handeln?
     c("k","kr","ks"), c("kt","ku","ab","f")
   ),2)
+
   
   # tasks in A2
   rules_a2 = matrix(list(
-    c(5), c(0,1,2,3,4,6),
+    c("5"), c("0","1","2","3","4","6"),
     c("k"), c("e","sp","p","g","nl","str","s","ab"),
-    c("k","1","2","p"), c("3","4","kl","g","s","div","ab"),
+    c("k","1","2","p"), c("3","4","5","kl","g","s","div","ab"),
     c("k","fk","ff","ub","d","n"), c("f","int"),
     c("k"), c("p","i","ab"),
     c("k","d"), c("p", "f"),
-    c(5), c(0,1,2,3,4,6,7,8,9,10,11,12),
-    c(2), c(1,0),
-    c(2), c(1,0),
-    c(1), c(2,0),
-    c(1), c(2,0),
-    c(1), c(2,0)
+    c("5"), c("0","1","2","3","4","6","7","8","9","10","11","12"),
+    c("2"), c("1","0"),
+    c("2"), c("1","0"),
+    c("1"), c("2","0"),
+    c("1"), c("2","0"),
+    c("1"), c("2","0")
   ),2)
   
   # tasks in B1
   rules_b1 = matrix(list(
-    c(2), c(1,0),
-    c(1), c(2,0),
-    c(2), c(1,0),
+    c("2"), c("1","0"),
+    c("1"), c("2","0"),
+    c("2"), c("1","0"),
     c("k"), c("p","i","g","nl","str","s","ab"),
-    c("k","1","2","sp"), c("3","4","kl","g","x","y","s","div","ab"),
+    c("k","1","2","sp"), c("3","4","5","kl","g","x","y","s","div","ab"),
     c("k","fk","ff","ub","d","n"), c("f"),
-    c(1), c(2,0),
-    c(1), c(2,0),
-    c(2), c(1,0),
-    c(2), c(1,0),
+    c("1"), c("2","0"),
+    c("1"), c("2","0"),
+    c("2"), c("1","0"),
+    c("2"), c("1","0"),
     c("k","ks"), c("u","f"),
     c("k","ks"), c("kt","ku","ab","f")
   ),2)
@@ -112,17 +143,17 @@ transform_codes <- function(){
   
   # tasks in B2
   rules_b2 = matrix(list(
-    c(2), c(0,1,3,4,5,6),
+    c("2"), c("0","1","3","4","5","6"),
     c("k"), c("e","sp","l","g","nl","str","s","ab"),
-    c("k","1","2","p"), c("3","4","kl","g","s","div","ab"),
+    c("k","1","2","p"), c("3","4","5","kl","g","s","div","ab"),
     c("k","ff","nb"), c("f","fk","n","int"),
     c("k","fp"), c("sp","i","ab"),
-    c(5), c(0,1,2,3,4,6,7,8,9),
-    c(2), c(1,0),
-    c(2), c(1,0),
-    c(1), c(2,0),
-    c(1), c(2,0),
-    c(1), c(2,0)
+    c("5"), c("0","1","2","3","4","6","7","8","9"),
+    c("2"), c("1","0"),
+    c("2"), c("1","0"),
+    c("1"), c("2","0"),
+    c("1"), c("2","0"),
+    c("1"), c("2","0")
   ),2)
   
   mapping_matrix = cbind(rules_a1, rules_a2, rules_b1, rules_b2)
@@ -139,24 +170,58 @@ transform_codes <- function(){
   for (i in 1:nrow(data)) {
     for (j in 1:ncol(data)) {
       # skip columns where multiple conditions must be met
-      if (j %in% c(5,6,16,17,18,19,30,31,40,41)){
+      if (j %in% c(5,6,12,16,17,18,19,30,31,40,41)){
         next
       }
       
       # TODO: Seperate multiple comma seperated entries
       # check whether entry evaluates to 1
-      if(data[i,j] %in% mapping_matrix["1",j]) {
+      #if(data[i,j] %in% unlist(mapping_matrix["1",j])) {
+      if (check_value(data[i,j],unlist(mapping_matrix["1",j]),FALSE)) {
         df[i,index_mapper[j]] = 1
       }
       
       # check whether entry evaluates to 0
-      if(data[i,j] %in% mapping_matrix["0",j]) {
+      #else if(data[i,j] %in% unlist(mapping_matrix["0",j])) {
+      else if (check_value(data[i,j],unlist(mapping_matrix["0",j]),TRUE)) {
         df[i,index_mapper[j]] = 0
       }
       
+      # keep original coded value if it cannot be mapped to 1 or 0
+      else {
+        df[i,index_mapper[j]] = data[i,j]
+      }
     }
-     
-    
-    return(df)
   }
+  
+  # Transform data were multiple conditions must be met
+  # TODO: was wenn es nicht drin ist und nur eins von beidem nicht drin ist? dann wird check value false beim check ob es in 0 ist
+  for (i in 1:nrow(data)) {
+    for (j in c(5, 16, 18, 30, 40)) {
+      # both conditions must be met to be evaluated to 1
+      #if(data[i,j] %in% unlist(mapping_matrix["1",j]) && data[i,j+1] %in% unlist(mapping_matrix["1",j+1])) {
+      if(check_value(data[i,j],unlist(mapping_matrix["1",j]),FALSE) && check_value(data[i,j+1],unlist(mapping_matrix["1",j+1]),FALSE)) {
+        df[i,index_mapper[j]] = 1
+      }
+      
+      # only one condition must be violated to be evaluated to 0
+      #else if(data[i,j] %in% unlist(mapping_matrix["0",j]) || data[i,j+1] %in% unlist(mapping_matrix["0",j+1])) {
+      else if(check_value(data[i,j],unlist(mapping_matrix["0",j]),TRUE) || check_value(data[i,j+1],unlist(mapping_matrix["0",j+1]),TRUE)) {
+        df[i,index_mapper[j]] = 0
+      }
+      
+      # keep merged original coded value if it cannot be mapped to 1 or 0
+      else {
+        if (data[i,j] == "na" && data[i,j+1] =="na") {
+          df[i,index_mapper[j]] = "na"
+        } else {
+          df[i,index_mapper[j]] = paste(data[i,j],data[i,j+1])
+        }
+      }
+    }
+    
+    
+  }
+  
+  return(df)
 }
